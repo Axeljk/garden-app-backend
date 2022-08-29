d6456af3b6359fa678f5d080fb951cd1778e717e
axeljk <axeljkern@yahoo.com>
2022-08-29 19:26:55 -0400

    Typos in models and index was missing the includes of each model.

---
+++ models/index.js
@@ -0,0 +1,6 @@
+const Layout = require("./Layout");
+const Plant = require("./Plant");
+const Profile = require("./Profile");
+const Species = require("./Species");
+
+module.exports = { Layout, Plant, Profile, Species };
