eb7edde83fcb8221957c454b1d27626c955649de
kitanablade <kiteagle@gmail.com>
2022-08-30 11:33:38 -0700

    refactoring profile routes to pull out logic into a controller file

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
-const profile = require("./profileRoutes");
-
-router.use("/profiles", profile);
+const profileRoutes = require("./profileRoutes");
+//const layoutRoutes = require("./layoutRoutes");
+const plantRoutes = require("./plantRoutes");
 
 module.exports = router;
 
 router.use('/profiles', profileRoutes);
-router.use('/layouts', layoutRoutes);
+//router.use('/layouts', layoutRoutes);
 router.use('/plants', plantRoutes);
